cc727eb4406e74ef12d7aadfb8b4f156915ebfe5
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-19 00:48:33 +0530

    function call in element

--- 8.JSX in React JS/proj1/src/App.js
+++ 8.JSX in React JS/proj1/src/App.js
@@ -3,9 +3,15 @@ import React from 'react'
 // const ele = <h1>Abhiram</h1>
 // const ele = <h1>{20+50}</h1>
 
-const name = "Abhiram"
+// const name = "Abhiram"
 
-const ele = <h1>Hello {name}</h1>
+// const ele = <h1>Hello {name}</h1>
 
+
+function show(name){
+    return name;
+}
+
+const ele = <h1>Hello {show("Abhi")}</h1>
 
 export default ele;
