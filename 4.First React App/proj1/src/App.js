d46505419ec7b00c81c682982de1c34f4b2ee103
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-18 17:09:45 +0530

    creating one component that component call in index.js file

---
+++ 4.First React App/proj1/src/App.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+
+let ele = <h1>Abhiram B S</h1>
+
+export default ele
--- 4.First React App/proj1/src/index.js
+++ 4.First React App/proj1/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
-
+import ele from './App'
 // let ele = React.createElement('h1', null, "Abhiram B S");
-let ele = <h1>Abhiram B S</h1>
+// let ele = <h1>Abhiram B S</h1>;
+
 ReactDOM.render(ele, document.getElementById("root"));
 
 
