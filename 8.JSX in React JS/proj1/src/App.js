a58d3a773a310595d73ed90c7c4527390ba8780c
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-19 00:53:20 +0530

    creating object and get that data in element completed

--- 8.JSX in React JS/proj1/src/App.js
+++ 8.JSX in React JS/proj1/src/App.js
@@ -8,10 +8,20 @@ import React from 'react'
 // const ele = <h1>Hello {name}</h1>
 
 
-function show(name){
-    return name;
+// function show(name){
+//     return name;
+// }
+
+// const ele = <h1>Hello {show("Abhi")}</h1>
+
+let user = {
+    firstname:"Abhiram",
+    lastname:"B S"
 }
 
-const ele = <h1>Hello {show("Abhi")}</h1>
+const ele = <h1>Hello {user.firstname}</h1>
+// const ele = <h1>Hello {user.lastname}</h1>
+
+
 
 export default ele;
