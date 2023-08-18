5c9d77012830ff178108c990542c5a184230c05f
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-19 00:45:49 +0530

    string data store in name and display in element created

--- 8.JSX in React JS/proj1/src/App.js
+++ 8.JSX in React JS/proj1/src/App.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 // const ele = <h1>Abhiram</h1>
-const ele = <h1>{20+50}</h1>
+// const ele = <h1>{20+50}</h1>
+
+const name = "Abhiram"
+
+const ele = <h1>Hello {name}</h1>
 
 
 export default ele;
