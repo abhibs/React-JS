b18b335767dbebce4cc0a66596811b1581844375
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-18 23:38:23 +0530

    creating two component Student and App in App component calling Student Multiple Component

---
+++ 7.Composing Component React Project/proj1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Student from './Student'
+
+const App = () =>{
+    return(
+        <>
+            <Student />
+            <Student />
+            <Student />
+        </>
+    )
+}
+
+export default App
