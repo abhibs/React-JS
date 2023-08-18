8478e7113e79f47b1fad83224c2461f72242aa9e
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-18 21:58:49 +0530

    functional component another type completed

--- 6.Functional Component and Class Component Example/proj1/src/Student.js
+++ 6.Functional Component and Class Component Example/proj1/src/Student.js
@@ -4,8 +4,8 @@ import React from 'react'
 //     return <h1>Hello Abhiram</h1>
 // }
 
-function Student(){
-    return <h1>Hello Abhiram</h1>
+function Student(props){
+    return <h1>Hello {props.name}</h1>
 }
 
 export default Student
--- 6.Functional Component and Class Component Example/proj1/src/index.js
+++ 6.Functional Component and Class Component Example/proj1/src/index.js
@@ -1,4 +1,4 @@
 import ReactDOM from 'react-dom'
 import Student from './Student'
 
-ReactDOM.render(<Student/>, document.getElementById('root'))
+ReactDOM.render(<Student name="Abhiram"/>, document.getElementById('root'))
