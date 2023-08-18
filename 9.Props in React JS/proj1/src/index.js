fdd00e91bc13df7992d56e231ee8a078034f8f13
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-19 02:36:43 +0530

    adding maths operation in render method

--- 9.Props in React JS/proj1/src/index.js
+++ 9.Props in React JS/proj1/src/index.js
@@ -2,4 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Student from './Student'
 
-ReactDOM.render(<Student name="Abhiram" roll="101"/>, document.getElementById('root'))
+// ReactDOM.render(<Student name="Abhiram" roll="101"/>, document.getElementById('root'))
+ReactDOM.render(<Student name="Abhiram" roll={100+1}/>, document.getElementById('root'))
