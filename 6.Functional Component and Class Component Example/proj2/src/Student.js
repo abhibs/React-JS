762d976f9582d6bd3413d01b5f91382cfc2b6ef8
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-18 22:19:43 +0530

    class component with using props

--- 6.Functional Component and Class Component Example/proj2/src/Student.js
+++ 6.Functional Component and Class Component Example/proj2/src/Student.js
@@ -1,8 +1,15 @@
 import React from 'react'
 
+// class Student extends React.Component{
+//     render(){
+//         return <h1>Hello Abhiram</h1>
+//     }
+// }
+
+
 class Student extends React.Component{
     render(){
-        return <h1>Hello Abhiram</h1>
+        return <h1>Hello {this.props.name}</h1>
     }
 }
 
--- 6.Functional Component and Class Component Example/proj2/src/index.js
+++ 6.Functional Component and Class Component Example/proj2/src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom'
 import Student from './Student'
 
-ReactDOM.render(<Student/>, document.getElementById('root'))
+// ReactDOM.render(<Student/>, document.getElementById('root'))
+ReactDOM.render(<Student name="Abhiram B S"/>, document.getElementById('root'))
