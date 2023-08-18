125173344a51763ce769ada1e38389d2d66d4830
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-18 16:56:15 +0530

    using tag through creating element

--- 4.First React App/proj1/src/index.js
+++ 4.First React App/proj1/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-let ele = React.createElement('h1', null, "Abhiram B S");
+// let ele = React.createElement('h1', null, "Abhiram B S");
+let ele = <h1>Abhiram B S</h1>
 ReactDOM.render(ele, document.getElementById("root"));
 
 
