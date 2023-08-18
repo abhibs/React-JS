62eed07cb3deee6abc5ca6724f7518a8caae7e3a
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-18 18:20:33 +0530

    anther way react frament created

--- 5.React Fragment/proj1/src/App.js
+++ 5.React Fragment/proj1/src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react'
 
 class App extends Component {
     render() {
+        // return (
+        //     <React.Fragment>
+        //         <h1>Abhiram</h1>
+        //         <p>Javalli</p>
+        //     </React.Fragment>
+        // );
+
         return (
-            <React.Fragment>
+            <>
                 <h1>Abhiram</h1>
                 <p>Javalli</p>
-            </React.Fragment>
+            </>
         );
     }
 }
