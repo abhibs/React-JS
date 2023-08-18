893dc168d26ad12fc4b3c51a96c5f07e760a8f3f
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-18 17:19:58 +0530

    changing component

--- 4.First React App/proj1/src/App.js
+++ 4.First React App/proj1/src/App.js
@@ -5,11 +5,13 @@ import React, {Component} from 'react'
 
 class App extends Component{
     render(){
-        return(
-            <div>
-                <h1>Abhiram B S</h1>
-            </div>
-        )
+        // return(
+        //     <div>
+        //         <h1>Abhiram B S</h1>
+        //     </div>
+        // )
+
+        return <h1>Abhiram B S</h1>
     }
 }
 
