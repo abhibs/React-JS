629c63e472cfddcb9982f63b920c71709f9a9a11
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-19 00:41:54 +0530

    string element created

---
+++ 8.JSX in React JS/proj1/src/App.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const ele = <h1>Abhiram</h1>
+
+export default ele;
---
+++ 8.JSX in React JS/proj1/src/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom'
+import ele from './App'
+
+ReactDOM.render(ele, document.getElementById('root'))
