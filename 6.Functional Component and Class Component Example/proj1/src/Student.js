9ad24253cac24f75919cdf6dd74690fa251b1237
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-18 21:55:53 +0530

    functional component one type completed

---
+++ 6.Functional Component and Class Component Example/proj1/src/Student.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+// function Student(){
+//     return <h1>Hello Abhiram</h1>
+// }
+
+function Student(){
+    return <h1>Hello Abhiram</h1>
+}
+
+export default Student
---
+++ 6.Functional Component and Class Component Example/proj1/src/index.js
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom'
+import Student from './Student'
+
+ReactDOM.render(<Student/>, document.getElementById('root'))
