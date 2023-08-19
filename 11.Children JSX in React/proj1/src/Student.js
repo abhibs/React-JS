1fa9f240089823d0f050b3427d923b197a44ae36
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-19 12:09:12 +0530

    children jsx completed in functional component

---
+++ 11.Children JSX in React/proj1/src/Student.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const Student = props => {
+    return <h1>Hello Abhiram {props.children}</h1>
+}
+
+export default Student
---
+++ 11.Children JSX in React/proj1/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Student from './Student'
+
+// ReactDOM.render(<Student>I am child</Student>, document.getElementById("root"));
+// ReactDOM.render(<Student> {101} </Student>, document.getElementById("root"));
+ReactDOM.render(<Student> {100+1} </Student>, document.getElementById("root"));
