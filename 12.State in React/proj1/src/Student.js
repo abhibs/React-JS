07d8e2f5a43ee78ee98c711fe3b7c5ee565b9438
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-19 18:08:46 +0530

    props varible call in state

--- 12.State in React/proj1/src/Student.js
+++ 12.State in React/proj1/src/Student.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 class Student extends React.Component{
+    // state = {
+    //     name:"Abhiram",
+    //     roll: 101
+    // }
+
     state = {
         name:"Abhiram",
-        roll: 101
+        roll: this.props.roll
     }
     render(){
         return <h1>Hello {this.state.name} and Roll Number: {this.state.roll}</h1>
--- 12.State in React/proj1/src/index.js
+++ 12.State in React/proj1/src/index.js
@@ -2,4 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Student from './Student'
 
-ReactDOM.render(<Student />, document.getElementById('root'))
+// ReactDOM.render(<Student />, document.getElementById('root'))
+ReactDOM.render(<Student roll={101}/>, document.getElementById('root'))
