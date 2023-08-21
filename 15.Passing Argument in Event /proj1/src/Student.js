e2f1e1599acfa91c0f44f83d9663181377aea1aa
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-21 18:10:28 +0530

    creating seperate function that fuction call the argument

--- 15.Passing Argument in Event /proj1/src/Student.js
+++ 15.Passing Argument in Event /proj1/src/Student.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react'
 
 export default class Student extends Component {
+    // state = {
+    //     name: "Abhiram"
+    // }
+
     state = {
+        id: 1,
         name: "Abhiram"
     }
 
@@ -9,14 +14,24 @@ export default class Student extends Component {
     //     console.log("Button Clicked");
     // }
 
+    // handleClick = (id) =>{
+    //     console.log(id);
+    // }
+
     handleClick = (id) =>{
         console.log(id);
     }
+
+    handleClickArg = () => {
+        this.handleClick(this.state.id)
+    }
+
     render() {
         return (
             <div>
                 <h1>Hello, {this.state.name}</h1>
-                <button onClick={this.handleClick("101")}>Click Me</button>
+                {/* <button onClick={this.handleClick("101")}>Click Me</button> */}
+                <button onClick={this.handleClickArg}>Click Me</button>
             </div>
         )
     }
