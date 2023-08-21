b73bb09b926d3f39c84442d611aca91981242ba0
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-21 18:03:31 +0530

    i am not click any button direcly call the value

--- 15.Passing Argument in Event /proj1/src/Student.js
+++ 15.Passing Argument in Event /proj1/src/Student.js
@@ -5,14 +5,18 @@ export default class Student extends Component {
         name: "Abhiram"
     }
 
-    handleClick = () =>{
-        console.log("Button Clicked");
+    // handleClick = () =>{
+    //     console.log("Button Clicked");
+    // }
+
+    handleClick = (id) =>{
+        console.log(id);
     }
     render() {
         return (
             <div>
                 <h1>Hello, {this.state.name}</h1>
-                <button onClick={this.handleClick}>Click Me</button>
+                <button onClick={this.handleClick("101")}>Click Me</button>
             </div>
         )
     }
