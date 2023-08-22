c7db282b57815e58f3229ff71574fef7bddb644b
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-22 21:02:19 +0530

    once click button update the value

--- 19.useState in React/proj1/src/App.js
+++ 19.useState in React/proj1/src/App.js
@@ -2,13 +2,40 @@ import React, {useState} from 'react'
 
 
 function App(){
-    const name = useState("Abhiram")
+    // const name = useState("Abhiram")
+    // function handleClick(){
+    //     name[1]("Abhi")
+    // }
+    // return(
+    //     <>
+    //         <h1>{name[0]}</h1>
+    //         <button onClick={handleClick}>Change</button>
+    //     </>
+    // )
+
+
+    // const name = useState("Abhiram")
+    // const data = name[0]
+    // const setName = name[1]
+    // function handleClick(){
+    //     setName("Abhi")
+    // }
+    // return(
+    //     <>
+    //         <h1>{data}</h1>
+    //         <button onClick={handleClick}>Change</button>
+    //     </>
+    // )
+
+
+    const [name, setName] = useState("Abhiram")
+    
     function handleClick(){
-        name[1]("Abhi")
+        setName("Abhi")
     }
     return(
         <>
-            <h1>{name[0]}</h1>
+            <h1>{name}</h1>
             <button onClick={handleClick}>Change</button>
         </>
     )
