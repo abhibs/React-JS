4e0ca8db0cf57c507161f1f4f1ddd8e59bce96a1
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-22 11:57:50 +0530

    getDerivedStateFromProps function using

--- 16.Mounting in React/proj1/src/App.js
+++ 16.Mounting in React/proj1/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-
+import Student from './Student'
 export default class App extends Component {
     // constructor(){
     //     super()
@@ -10,8 +10,23 @@ export default class App extends Component {
         super(props)
         console.log("App Constructor Called");
         console.log(props);
+
+        this.state = {
+            roll: "101"
+        }
     }
+
+    static getDerivedStateFromProps(props, state){
+        console.log("app get derived state from props");
+        console.log(props, state);
+        return null
+    }
+
     render() {
-        return <h1>Hello Abhiram</h1>
+        return(
+            <>
+                <Student name="Abhiram"/>
+            </>
+        )
     }
 }
