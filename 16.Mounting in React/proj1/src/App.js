d2d4cae9c76f656d1b67b4a2278ec5126c08bdb4
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-22 11:45:58 +0530

    contructor calling with props

--- 16.Mounting in React/proj1/src/App.js
+++ 16.Mounting in React/proj1/src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 
 export default class App extends Component {
-    constructor(){
-        super()
+    // constructor(){
+    //     super()
+    //     console.log("App Constructor Called");
+    // }
+
+    constructor(props){
+        super(props)
         console.log("App Constructor Called");
+        console.log(props);
     }
     render() {
         return <h1>Hello Abhiram</h1>
