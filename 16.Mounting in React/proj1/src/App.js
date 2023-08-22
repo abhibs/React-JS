c5099ee5380a121c26fb27efa938f7e1559178e9
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-22 11:41:24 +0530

    contructor calling without props

---
+++ 16.Mounting in React/proj1/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    constructor(){
+        super()
+        console.log("App Constructor Called");
+    }
+    render() {
+        return <h1>Hello Abhiram</h1>
+    }
+}
