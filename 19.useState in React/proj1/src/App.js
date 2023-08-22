c44c9d79310757ed1d2d405d1c643fd3d15f8ef0
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-22 20:56:34 +0530

    update the use state variable through array value 1

--- 19.useState in React/proj1/src/App.js
+++ 19.useState in React/proj1/src/App.js
@@ -3,10 +3,13 @@ import React, {useState} from 'react'
 
 function App(){
     const name = useState("Abhiram")
-
+    function handleClick(){
+        name[1]("Abhi")
+    }
     return(
         <>
             <h1>{name[0]}</h1>
+            <button onClick={handleClick}>Change</button>
         </>
     )
 }
