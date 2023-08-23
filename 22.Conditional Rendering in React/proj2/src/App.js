2cf47323e9f23720d4c4e922a9bc5232a77b390b
abhibs <abhiram.bs@webdigitalmantra.in>
2023-08-23 12:20:48 +0530

    conditional rendering with logical operator completed

---
+++ 22.Conditional Rendering in React/proj2/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import User from "./User";
+
+export default class App extends Component {
+    render() {
+        const primeMember = this.props.primeMember;
+
+        return (
+            <>
+                <h1>Welcome User </h1>
+                {primeMember && <User />}
+            </>
+        )
+    }
+}
