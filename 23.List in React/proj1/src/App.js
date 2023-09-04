9f4d71197525998966bf734d4470b63848e69d47
abhibs <abhiram.bs@webdigitalmantra.in>
2023-09-05 01:19:46 +0530

    in jsx calling map function

--- 23.List in React/proj1/src/App.js
+++ 23.List in React/proj1/src/App.js
@@ -3,15 +3,20 @@ import React, { Component } from 'react'
 export default class App extends Component {
     render() {
         const arr = [10, 20, 30, 40]
-        console.log("Old Array :", arr);
-        const newArr = arr.map((num)=>{
-            console.log("Num: ", num);
-            return <li name="Abhiram">{num * 2}</li>
-        })
-        console.log("New Array: ", newArr);
+        // console.log("Old Array :", arr);
+        // const newArr = arr.map((num)=>{
+        //     console.log("Num: ", num);
+        //     return <li name="Abhiram">{num * 2}</li>
+        // })
+        // console.log("New Array: ", newArr);
         return (
         <ul>
-            {newArr}
+            {/* {newArr} */}
+            {
+                arr.map(num=>{
+                    return <li>{num}</li>
+                })
+            }
         </ul>
         )
     }
