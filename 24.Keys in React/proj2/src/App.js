1aea3654eeeefcbd4129a8d5c5c06a20075a149b
abhibs <abhiram.bs@webdigitalmantra.in>
2023-09-05 04:28:41 +0530

    keys is used in user component

---
+++ 24.Keys in React/proj2/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import User from './User'
+export default class App extends Component {
+    render() {
+        const arr = this.props.numbers
+        const newArr = arr.map(num =>{
+            return <User key={num} value={num}/>
+        })
+        return (
+            <>
+                <ul>{newArr}</ul> 
+            </>
+        )
+    }
+}
