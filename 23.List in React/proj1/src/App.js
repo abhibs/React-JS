6dc3ef7c297ade526380930eabfe161e0a0922a0
abhibs <abhiram.bs@webdigitalmantra.in>
2023-09-05 01:11:57 +0530

    in console getting element

--- 23.List in React/proj1/src/App.js
+++ 23.List in React/proj1/src/App.js
@@ -3,10 +3,12 @@ import React, { Component } from 'react'
 export default class App extends Component {
     render() {
         const arr = [10, 20, 30, 40]
-
+        console.log("Old Array :", arr);
         const newArr = arr.map((num)=>{
+            console.log("Num: ", num);
             return <li>{num * 2}</li>
         })
+        console.log("New Array: ", newArr);
         return (
         <ul>
             {newArr}
