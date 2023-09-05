f58d3892a3e737e609acf1d37cbb9859f617356f
abhibs <abhiram.bs@webdigitalmantra.in>
2023-09-05 20:45:55 +0530

    inside src folder adding image

---
+++ 28. Image or Assets in React/proj2/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import abhi from './abhi.jpg'
+export default class App extends Component {
+    render() {
+        return (
+            <>
+                <img src={abhi} alt="myPic" width="300px"></img>
+            </>
+        )
+    }
+}
