928ea1df82de1d3cca3bfe07747cba8d90a66fd9
abhibs <abhiram.bs@webdigitalmantra.in>
2023-09-05 18:17:45 +0530

    spread operator in inline css and one for variable css and another direct call

--- 25.Inline CSS in React/proj1/src/App.js
+++ 25.Inline CSS in React/proj1/src/App.js
@@ -7,17 +7,23 @@ export default class App extends Component {
         //     backgroundColor: 'orange'
         // }
 
+        // const txtColor = {
+        //     color: "blue"
+        // }
+
+        // const txtFont = {
+        //     fontSize: "80px"
+        // }
+
         const txtColor = {
             color: "blue"
-        }
-
-        const txtFont = {
-            fontSize: "80px"
         }
         return (
         <div>
             {/* <button style={btnStyle}>Click Me</button> */}
-            <h1 style={{ ...txtColor, ...txtFont }}>Abhiram B S</h1>
+            {/* <h1 style={{ ...txtColor, ...txtFont }}>Abhiram B S</h1> */}
+            <h1 style={{ ...txtColor, ...{fontSize:"100px"} }}>Abhiram B S</h1>
+
         </div>
         )
     }
