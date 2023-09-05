30686e44722c5fa17d14c693d3098422cecd06b3
abhibs <abhiram.bs@webdigitalmantra.in>
2023-09-05 19:10:39 +0530

    2 component created and 2 css file created

---
+++ 26.External Stylesheet in Reat/proj2/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import User from './User'
+import './App.css'
+export default class App extends Component {
+    render() {
+        return (
+            <>
+                <h1 className='txt1'>React JS</h1>
+                <User />
+            </>
+        )
+    }
+}
---
+++ 26.External Stylesheet in Reat/proj2/src/User.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './User.css'
+
+
+export default class User extends Component {
+    render() {
+        return (
+            <>
+                <h2 className='txt2'>Abhiram</h2>
+            </>
+        )
+    }
+}
