b146f61ad727c3e06be2632a3d7fbba726d927c9
abhibs <abhiram.bs@webdigitalmantra.in>
2023-09-05 23:38:07 +0530

    bootstrap package installing

---
+++ 29.Bootstrap in React/proj2/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <button className='btn btn-primary'> Click Me </button>
+            </div>
+        )
+    }
+}
