71ec1eaa16b36530e1bfd1e4dc44aaa96c008f58
abhibs <abhiram.bs@webdigitalmantra.in>
2023-09-05 18:14:07 +0530

    how to call two inline css file

--- 25.Inline CSS in React/proj1/src/App.js
+++ 25.Inline CSS in React/proj1/src/App.js
@@ -2,13 +2,22 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
     render() {
-        const btnStyle = {
-            color: "blue",
-            backgroundColor: 'orange'
+        // const btnStyle = {
+        //     color: "blue",
+        //     backgroundColor: 'orange'
+        // }
+
+        const txtColor = {
+            color: "blue"
+        }
+
+        const txtFont = {
+            fontSize: "80px"
         }
         return (
         <div>
-            <button style={btnStyle}>Click Me</button>
+            {/* <button style={btnStyle}>Click Me</button> */}
+            <h1 style={{ ...txtColor, ...txtFont }}>Abhiram B S</h1>
         </div>
         )
     }
