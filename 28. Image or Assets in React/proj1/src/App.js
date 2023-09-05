a4e518e997323cde62199018d34b32106c2ed8ef
abhibs <abhiram.bs@webdigitalmantra.in>
2023-09-05 20:30:47 +0530

    in app.js file calling that image

---
+++ 28. Image or Assets in React/proj1/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <img src={process.env.PUBLIC_URL + "/abhi.jpg" } alt="myPic" width="300px"></img>
+            </div>
+        )
+    }
+}
