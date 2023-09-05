e4e1a5fe632edb043e1035b8b538758317400f2c
abhibs <abhiram.bs@webdigitalmantra.in>
2023-09-05 20:32:20 +0530

    seperate image folder created

--- 28. Image or Assets in React/proj1/src/App.js
+++ 28. Image or Assets in React/proj1/src/App.js
@@ -4,7 +4,7 @@ export default class App extends Component {
     render() {
         return (
             <div>
-                <img src={process.env.PUBLIC_URL + "/abhi.jpg" } alt="myPic" width="300px"></img>
+                <img src={process.env.PUBLIC_URL + "images/abhi.jpg" } alt="myPic" width="300px"></img>
             </div>
         )
     }
