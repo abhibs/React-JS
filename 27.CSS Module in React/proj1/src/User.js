23304ab9b1b1cea67a7fd159b777e280bbf76233
abhibs <abhiram.bs@webdigitalmantra.in>
2023-09-05 19:57:38 +0530

    in user component using app.module.css class name

--- 27.CSS Module in React/proj1/src/User.js
+++ 27.CSS Module in React/proj1/src/User.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import Styles from './User.module.css'
+import Styles2 from './App.module.css'
 export default class User extends Component {
     render() {
         return (
             <>
                 <h2 className={Styles.txt}>Javalli Tudoor Thirthahalli</h2>
+                <h3 className={Styles2.txt}>MCA</h3>
             </>
         )
     }
